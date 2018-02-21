5650b468fbb607c8f5ff8010431ae603ebd43857
Alexandre ANNOUNE <aannoune@ip-label.com>
2018-02-21 15:55:24 +0100

    No need to serve the old flavoured swagger.. new swagger at /doc. Basic implementation of the post method with some comments. Yaml file fix: post accept formdData instead of body .

--- api/controllers/file.js
+++ api/controllers/file.js
@@ -1,5 +1,21 @@
 //const uploadDir = `${__dirname}/uploads`
+const fs = require('fs')
 
+const uploadFile = (req, res, next) =>{
+	console.log(req.file)
+	fs.rename(req.file.path,`${process.cwd()}/uploads/${req.file.originalname}`, (err)=>{
+		if(err) {
+			res.status(500).send('Upload Error')
+			console.log(err)
+		}
+		else res.status(200).send({id:null,name:req.file.originalname,extension:null,uploaddate:null})
+		// A noter que je n'ai pas codé la recupération de toutes les données.. 
+		// Vous devriez noter que le fait d'uploader un nouveau fichier perturbera l'attribution des ID: 
+		// On ne peut pas se baser uniquement par leur ordre alphabetique, a moins de les renommer dans un format qui enregistre l'id (id_nomfifier.ext )
+		// Evidement la solution réside dans le fait de conserver une "base de donnée " a minimuàm sous la forme d'un fichier json
+		
+})
+}
 const db = require('../../config/db')()
 
 //GET /files operationId
@@ -15,4 +31,4 @@ const downloadFile = (req, res, next) => {
     : res.status(400).send('Invalid ID supplied')
 }
 
-module.exports = {resumeFiles, getInfoFile, downloadFile}
+module.exports = {resumeFiles, getInfoFile, downloadFile,uploadFile}
--- server.js
+++ server.js
@@ -6,15 +6,19 @@ const yaml = require('yamljs')
 // const fileUpload = require('express-fileupload')
 
 const file = require('./api/controllers/file.js')
-
+const bodyParser = require('body-parser');
 const publicDir = `${__dirname}/public`
 const swaggerdoc = yaml.load(`${publicDir}/swaggerdoc.yaml`)
 
+// Utilisation de multer : Quant on upload un fichier on le fait souvent via un post encodé en  multipart/form-data
+// Pour votre décharge il y avait une legere erreure dans la definition swagger a ce sujet... 
+const multer  = require('multer');
+const uploadMulter = multer({ dest: `${publicDir}/_uploadtmp/` })
 const port = process.env.PORT || 3000
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', port)
 
-app.use(express.static(publicDir))
+//app.use(express.static(publicDir))
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerdoc, {}))
 // app.use(fileUpload())
 
@@ -28,8 +32,9 @@ app.get('/files/:file_id/infos', file.getInfoFile)
 // /files/{file_id}
 app.get('/files/:file_id', file.downloadFile)
 
-// POST /files/post
-//app.post('/files/post', file.uploadFile)
+// POST /files/post  
+//ici le middleware d'express
+app.post('/files/post',uploadMulter.single('fileupload'), file.uploadFile)
 
 // Listen for requests
 const server = app.listen(app.get('port'), function() {
